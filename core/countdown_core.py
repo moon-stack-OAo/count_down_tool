0cbdcfd67baf23bb921c48eeac44ef37b2f702f4
Moon <moon1002.x@gmail.com>
2026-09-18 17:36:17 +0800

    chore(release): 1.4.5
    Mini 左侧时钟与目标时间双行显示。

--- core/countdown_core.py
+++ core/countdown_core.py
@@ -11,7 +11,7 @@ import sys
 from datetime import datetime, timedelta
 from typing import Any, Dict, Optional, Tuple, Union
 
-__version__ = "1.4.4"
+__version__ = "1.4.5"
 APP_NAME = "倒计时工具"
 APP_NAME_EN = "Count Down Tool"
 
